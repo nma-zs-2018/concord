cf3be4e01d1a15808bf3272b7bd965ee093988e2
Dekedro <dekedro@protonmail.com>
2018-02-19 23:35:57 +0000

    Remove something not important

--- src/concord/models.py
+++ src/concord/models.py
@@ -7,15 +7,9 @@ class User(AbstractUser):
     key_private_crypted = models.TextField()
 
 class Group(models.Model):
-    def __str__(self):
-        return self.group_name
-
     group_name = models.TextField()
 
 class UserGroupInfo(models.Model):
-    def __str__(self):
-        return str(self.group)
-
     group = models.ForeignKey(Group, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     key_symmetric_crypted = models.TextField()
